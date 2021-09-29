2b90bc70b63b6010f12b51fdbbeaa83279ccf85b
Jonathan Leibovich <leibo023@umn.edu>
2021-09-28 23:49:51 -0500

    Euclidean Algorithm works && gitp

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import math
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
+def find_gcd(x, n):
+    multipliers = []
+    i = 0
+    while x != 0:
+        temp = x
+        multipliers.append(math.floor(n / x))
+        x = (n % x)
+        print("Step ", i, ": ", n, " = ", multipliers[i], "(", temp, ")", " + ", x, sep='')
+        n = temp
+        i += 1
+    return n
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    while 1:
+        x = int(input("Please input the first number you want to find the gcd for: "))
+        n = int(input("Please input the other number you want to find the gcd for: "))
+        print("You would like to find the gc of ", x, " and ", n, "? [y/n] ", sep='', end='')
+        response = input()
+        if response == "y" or response == "yes":
+            gcd = find_gcd(x,n)
+            if x % gcd == 0 and n % gcd == 0:
+                print("The GCD is: ", gcd, "!", sep='')
+                break
+            else:
+                print("Unfortunately the GCD could not be calculated")
